e78ac60dbafcfca20f2ec84bff559536ed5a9089
Hristo Spasov <hristo_sp99@abv.bg>
2017-09-02 18:02:34 +0300

    Implement sending file metadata to server.

--- app/pages/datawatcher.page.js
+++ app/pages/datawatcher.page.js
@@ -7,6 +7,7 @@ class DataWatcher extends React.Component {
         this.state = {
             selectedRootDirectory: '',
             currentDirectory: '',
+            scannedFiles: new Map(),
             watcher: null,
             watcherOptions: {
                 ignored: /[\/\\]\./,
@@ -23,16 +24,26 @@ class DataWatcher extends React.Component {
 
         this.handleSelectRootDirectory = this.handleSelectRootDirectory.bind(this);
         this.scanDirectory = this.scanDirectory.bind(this);
+        this.addToScannedFiles = this.addToScannedFiles.bind(this);
+        this.removeFromScannedFiles = this.removeFromScannedFiles.bind(this);
+        this.getFileMetadata = this.getFileMetadata.bind(this);
+        this.getFilePermissions = this.getFilePermissions.bind(this);
     }
 
     componentWillUnmount() {
         this.state.watcher.close();
         this.state.socket.disconnect();
-        this.setState({ selectedRootDirectory: '', watcher: null, socket: null });
+        this.setState({
+            selectedRootDirectory: '',
+            currentDirectory: '',
+            scannedFiles: null,
+            watcher: null,
+            socket: null
+        });
     }
 
     scanDirectory() {
-        const path = this.state.selectedRootDirectory;
+        const path = this.state.currentDirectory;
         const chokidar = window.require("chokidar");
 
         if (this.state.watcher !== null) {
@@ -50,34 +61,123 @@ class DataWatcher extends React.Component {
         }
 
         this.state.watcher
+            .on('raw', function (event, path, details) {
+                // This event should be triggered everytime something happens.
+                console.log('Raw event info:', event, path, details);
+            })
             .on('add', path => {
-                this.state.socket.emit('demo', `File ${path} has been added`);
+                this.addToScannedFiles(path);
+                this.state.socket.emit('demo', `File ${path} has been added.`, this.state.scannedFiles.get(path));
                 console.log('File', path, 'has been added');
             })
             .on('addDir', path => {
-                this.state.socket.emit('demo', `Directory ${path} has been added`);
+                this.addToScannedFiles(path);
+                this.state.socket.emit('demo', `Directory ${path} has been added`, this.state.scannedFiles.get(path));
                 console.log('Directory', path, 'has been added');
             })
             .on('change', path => {
-                this.state.socket.emit('demo', `File ${path} has been changed`);
+                this.addToScannedFiles(path);
+                this.state.socket.emit('demo', `File ${path} has been changed`, this.state.scannedFiles.get(path));
                 console.log('File', path, 'has been changed');
             })
             .on('unlink', path => {
+                this.removeFromScannedFiles(path);
                 this.state.socket.emit('demo', `File ${path} has been removed`);
                 console.log('File', path, 'has been removed');
             })
             .on('unlinkDir', path => {
+                this.removeFromScannedFiles(path);
                 this.state.socket.emit('demo', `Directory ${path} has been changed`);
                 console.log('Directory', path, 'has been removed');
             })
             .on('error', error => {
                 console.log('Error happened', error);
             })
-            .on('ready', onWatcherReady)
-            .on('raw', function (event, path, details) {
-                // This event should be triggered everytime something happens.
-                console.log('Raw event info:', event, path, details);
-            });
+            .on('ready', onWatcherReady);
+    }
+
+    addToScannedFiles(path) {
+        let fileMetadata = this.getFileMetadata(path);
+        console.log(fileMetadata);
+        this.state.scannedFiles.set(path, fileMetadata);
+    }
+
+    removeFromScannedFiles(path) {
+        this.state.scannedFiles.delete(path);
+    }
+
+    getFileType(path) {
+        const fs = window.require('fs');
+        let result = "error";
+        let stats = fs.lstatSync(path);
+        if (stats.isBlockDevice()) {
+            result = "blockDevice";
+        } else if (stats.isCharacterDevice()) {
+            result = "characterDevice";
+        } else if (stats.isDirectory()) {
+            result = "directory";
+        } else if (stats.isFIFO()) {
+            result = "fifo";
+        } else if (stats.isFile()) {
+            result = "file";
+        } else if (stats.isSocket()) {
+            result = "socket";
+        } else if (stats.isSymbolicLink()) {
+            result = "symbolicLink";
+        }
+        return result;
+    }
+
+    canRead(path) {
+        const fs = window.require('fs');
+        try {
+            fs.accessSync(path, fs.constants.R_OK);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    canWrite(path) {
+        const fs = window.require('fs');
+        try {
+            fs.accessSync(path, fs.constants.W_OK);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    canExecute(path) {
+        const fs = window.require('fs');
+        try {
+            fs.accessSync(path, fs.constants.X_OK);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    getFilePermissions(path) {
+        let filePermissions = {};
+        filePermissions.read = this.canRead(path);
+        filePermissions.write = this.canWrite(path);
+        filePermissions.execute = this.canWrite(path);
+        console.log(filePermissions);
+        return filePermissions;
+    }
+
+    getFileMetadata(path) {
+        const pathModule = window.require('path');
+        const fs = window.require('fs');
+        let metadata = {};
+        let stats = fs.lstatSync(path);
+        // size in bytes:
+        metadata.name = pathModule.basename(path);
+        metadata.type = this.getFileType(path);
+        metadata.size = stats["size"];
+        metadata.access = this.getFilePermissions(path);
+        return metadata;
     }
 
     _addDirectory(node) {
@@ -95,7 +195,11 @@ class DataWatcher extends React.Component {
     }
 
     handleSelectRootDirectory(event) {
-        this.setState({ selectedRootDirectory: event.target.files[0].path });
+        let dirPath = event.target.files[0].path;
+        this.setState({
+            selectedRootDirectory: dirPath,
+            currentDirectory: dirPath
+        });
     }
 
     render() {
