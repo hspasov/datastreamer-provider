e0c3b2601f7c33d92062f919c9b603314e67de0f
Hristo Spasov <hristo_sp99@abv.bg>
2017-09-21 16:16:34 +0300

    Stability improvements: Automatically reset p2p connection when attempt fails

--- app/pages/datawatcher.page.js
+++ app/pages/datawatcher.page.js
@@ -21,67 +21,14 @@ class DataWatcher extends React.Component {
 
         this.socket.on("unsubscribedClient", clientId => {
             let client = this.clients.get(clientId);
-            if (client) {
-                client.delete();
-                client.sendChannel && client.sendChannel.close();
-                console.log("Closed data channel with label: " + client.sendChannel.label)
-                client.receiveChannel && client.receiveChannel.close();
-                console.log("Closed data channel with label: " + client.receiveChannel.label);
-                client.peerConnection && client.peerConnection.close();
-                console.log("Closed peer connection");
-                this.clients.delete(clientId);
-            }
+            this.deleteP2PConnection(client);
         });
 
         this.socket.on("subscribedClient", clientId => {
             this.clients.set(clientId, new Client(clientId));
             console.log("Just set clientId", clientId);
             const client = this.clients.get(clientId);
-            try {
-                client.servers = null;
-                client.peerConnectionConstraint = null;
-                client.dataConstraint = null;
-                client.peerConnection = new RTCPeerConnection(client.servers, client.peerConnectionConstraint);
-                console.log("Created local peer connection object localConnection");
-                client.peerConnection.onicecandidate = event => {
-                    console.log("local ice callback");
-                    if (event.candidate) {
-                        this.socket.emit("sendICECandidate", "client", this.props.provider.providerId, event.candidate);
-                    }
-                };
-
-                client.peerConnection.ondatachannel = event => {
-                    console.log("Receive Channel Callback");
-                    client.receiveChannel = event.channel;
-                    client.receiveChannel.onmessage = event => {
-                        console.log("receive message: ", JSON.parse(event.data));
-                        this.processMessage(client, JSON.parse(event.data));
-                    }
-                }
-
-                client.receiveChannel = client.peerConnection.createDataChannel("receiveDataChannel", client.dataConstraint);
-                client.sendChannel = client.peerConnection.createDataChannel("sendDataChannel", client.dataConstraint);
-                console.log("Created send data channel");
-
-                client.sendChannel.onopen = () => {
-                    console.log("send channel state is: " + client.sendChannel.readyState);
-                    client.sendChannel.send(JSON.stringify({
-                        action: "message",
-                        message: "It works, from provider"
-                    }));
-                    if (this.selectedRootDirectory) {
-                        this.scanDirectory(client);
-                    }
-                }
-                console.log("Requesting P2P connection");
-                this.socket.emit("requestP2PConnection", clientId);
-            } catch (e) {
-                if (!client || !client.sendChannel || !client.receiveChannel || !client.peerConnection) {
-                    console.log("Connection with client lost");
-                } else {
-                    throw e;
-                }
-            }
+            this.initializeP2PConnection(client);
         });
 
         this.socket.on("initConnection", (clientId, description) => {
@@ -98,6 +45,7 @@ class DataWatcher extends React.Component {
                     },
                     error => {
                         console.log("there was an error while creating an answer", error);
+                        this.deleteP2PConnection(client, error);
                     }
                 );
             } catch (e) {
@@ -107,6 +55,12 @@ class DataWatcher extends React.Component {
                     throw e;
                 }
             }
+        });
+
+        this.socket.on("resetConnection", clientId => {
+            const client = this.clients.get(clientId);
+            this.deleteP2PConnection(client);
+            this.initializeP2PConnection(client);
         });
 
         this.socket.on("receiveICECandidate", (clientId, candidate) => {
@@ -120,6 +74,7 @@ class DataWatcher extends React.Component {
                     },
                     error => {
                         console.log("failed to add candidate", error);
+                        this.deleteP2PConnection(client, error);
                     }
                 );
                 console.log("Local ICE candidate: \n " + candidate);
@@ -137,7 +92,9 @@ class DataWatcher extends React.Component {
         this.sendMessage = this.sendMessage.bind(this);
         this.processMessage = this.processMessage.bind(this);
         this.initializeScan = this.initializeScan.bind(this);
+        this.initializeP2PConnection = this.initializeP2PConnection.bind(this);
         this.scanDirectory = this.scanDirectory.bind(this);
+        this.deleteClientSession = this.deleteP2PConnection.bind(this);
         this.deleteSession = this.deleteSession.bind(this);
     }
 
@@ -159,9 +116,74 @@ class DataWatcher extends React.Component {
         this.socket.emit("connectToClients", this.props.provider.providerId);
     }
 
+    initializeP2PConnection(client) {
+        try {
+            client.servers = null;
+            client.peerConnectionConstraint = null;
+            client.dataConstraint = null;
+            client.peerConnection = new RTCPeerConnection(client.servers, client.peerConnectionConstraint);
+            console.log("Created local peer connection object localConnection");
+            client.peerConnection.onicecandidate = event => {
+                console.log("local ice callback");
+                if (event.candidate) {
+                    this.socket.emit("sendICECandidate", "client", this.props.provider.providerId, event.candidate);
+                }
+            };
+
+            client.peerConnection.ondatachannel = event => {
+                console.log("Receive Channel Callback");
+                client.receiveChannel = event.channel;
+                client.receiveChannel.onmessage = event => {
+                    console.log("receive message: ", JSON.parse(event.data));
+                    this.processMessage(client, JSON.parse(event.data));
+                }
+            }
+
+            client.receiveChannel = client.peerConnection.createDataChannel("receiveDataChannel", client.dataConstraint);
+            client.sendChannel = client.peerConnection.createDataChannel("sendDataChannel", client.dataConstraint);
+            console.log("Created send data channel");
+
+            client.sendChannel.onopen = () => {
+                console.log("send channel state is: " + client.sendChannel.readyState);
+                client.sendChannel.send(JSON.stringify({
+                    action: "message",
+                    message: "It works, from provider"
+                }));
+                if (this.selectedRootDirectory) {
+                    this.scanDirectory(client);
+                }
+            }
+            console.log("Requesting P2P connection");
+            this.socket.emit("requestP2PConnection", client.id);
+        } catch (e) {
+            if (!client || !client.sendChannel || !client.receiveChannel || !client.peerConnection) {
+                console.log("Connection with client lost");
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    deleteP2PConnection(client, error = null) {
+        if (client) {
+            client.delete();
+            client.sendChannel && client.sendChannel.close();
+            console.log("Closed data channel with label: " + client.sendChannel.label)
+            client.receiveChannel && client.receiveChannel.close();
+            console.log("Closed data channel with label: " + client.receiveChannel.label);
+            client.peerConnection && client.peerConnection.close();
+            console.log("Closed peer connection");
+            this.clients.delete(client.id);
+            if (error) {
+                this.socket.emit("resetClientConnection", client.id);
+            }
+        }
+    }
+
     deleteSession() {
         this.socket.disconnect();
         this.clients.forEach((client, clientId) => {
+            this.deleteP2PConnection(client);
             client.delete();
         });
     }
